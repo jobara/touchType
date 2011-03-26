249ddff85e5d777c5f936a795d30c405ba47a5d2
Justin Obara <obarajustin@gmail.com>
2011-03-25 23:40:19 -0400

    GH-2 : Starting the creator function
    Added the creator function for typingTest. Also added the first unit test. There was a missing directory in lib, infusion. Added this in as well.

--- src/component/js/touchType.js
+++ src/component/js/touchType.js
@@ -4,3 +4,20 @@ Copyright 2011 Justin Obara
 Licensed under the MIT License. You may not use this file except 
 in compliance with this License.
 */
+
+// Declare Dependencies
+/*global tt, jQuery*/
+
+var tt = tt || {};
+
+(function ($) {
+    tt.typingTest = function (container, options) {
+        var that = fluid.initView("tt.typingTest", container, options);
+        
+        return that;
+    };
+    
+    fluid.defaults("tt.typingTest", {
+        
+    });
+})(jQuery);
--- src/tests/componentTests/js/touchType-tests.js
+++ src/tests/componentTests/js/touchType-tests.js
@@ -4,3 +4,23 @@ Copyright 2011 Justin Obara
 Licensed under the MIT License. You may not use this file except 
 in compliance with this License.
 */
+
+// Declare dependencies
+/*global tt, jQuery, jqUnit*/
+
+(function ($) {
+    var ttTests = new jqUnit.TestCase("touchType Tests");
+    
+    
+    var createTypingTest = function (options) {
+        return tt.typingTest("#typingTest", options);
+    };
+    
+    $(document).ready(function () {
+    
+        ttTests.test("Initialization", function () {
+            jqUnit.assertTrue("typingTest initialized", createTypingTest());
+        });
+        
+    });
+})(jQuery);
